8fd5ad77140da13339f3dcc3fdbf916ee9a92104
Cole Winchester <cowinch@gmail.com>
2023-09-05 15:00:41 -0500

    offset determined by window.scroll JANKY

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("linked")
+console.log(window.innerHeight)
+console.log(`innerWidth: ${window.innerWidth}`)
+let totalLeftScroll=200
+window.onscroll = function(e) {
+    // window.scrollX=(window.scrollY/window.innerHeight)*totalLeftScroll
+    console.log(window.scrollX)
+    window.scrollTo(((window.scrollY/window.innerHeight)*totalLeftScroll),window.scrollY)
+}
